24e66f4a6daf95752336250424ebb1170a777012
Gaurav <gmangal@email.arizona.edu>
2013-07-22 21:19:02 -0700

    Created a printCube method in the model for testing purposes. Created a test class to test the model. Changed the .gitignore back so it excludes package files again

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -529,4 +529,15 @@ public class RubiksCube {
 		}
 		return true;
 	}
+	
+	public void printCube() {
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 3; k++) {
+					System.out.print(cube[i][j][k]);
+				}
+				System.out.print("\n");
+			}
+		}
+	}
 } // class RubiksCube
